da4776753acd80490551a7d59915fabe4e2cb72e
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-06-25 19:08:24 +0530

    offers

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+
+function hug() {
+
+  var a = document.getElementById('i')
+
+
+  document.getElementById('i').style.display = 'block'
+
+  document.a.style.display = 'block'
+
+}
